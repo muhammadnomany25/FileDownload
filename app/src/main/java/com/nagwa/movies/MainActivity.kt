ba9a497870127c84c14660a813e4cab2228dfc69
Muhammad Noamany <muhammadnoamany@gmail.com>
2022-02-01 22:32:39 +0200

    ## initial commit

---
+++ app/src/main/java/com/nagwa/movies/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.nagwa.movies
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
